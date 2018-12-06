cbf57fbfcf78d6e9528040110deebb247f9c3f9a
baobingo <novikov.inbox@gmail.com>
2018-12-06 15:30:39 +0300

    PR#1 added Quiz interface

--- spring01/src/main/java/ru/otus/spring01/Main.java
+++ spring01/src/main/java/ru/otus/spring01/Main.java
@@ -2,7 +2,7 @@ package ru.otus.spring01;
 
 public class Main {
     public static void main(String[] args) {
-        Quiz quiz = new Quiz();
+        Quiz quiz = new SimpleQuiz();
         quiz.startQuiz();
     }
 }
--- spring01/src/main/java/ru/otus/spring01/Quiz.java
+++ spring01/src/main/java/ru/otus/spring01/Quiz.java
@@ -1,33 +1,5 @@
 package ru.otus.spring01;
 
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import ru.otus.spring01.service.QuizService;
-import ru.otus.spring01.ui.Console;
-import ru.otus.spring01.ui.SimpleUI;
-
-import java.util.stream.IntStream;
-
-public class Quiz {
-    public void startQuiz(){
-        String name;
-        String surname;
-
-        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("/context.xml");
-        QuizService service = context.getBean(QuizService.class);
-        Console console = context.getBean(SimpleUI.class);
-
-        console.write("Let's play a game with you \nWhat's you name?");
-        name = console.read();
-        console.write("What's you family name?");
-        surname = console.read();
-
-        service.startQuiz(name, surname);
-
-        IntStream.range(0, service.questionCount()).forEach(i ->{
-            console.write(service.getQuestion());
-            service.checkAnswer(console.read());
-        });
-
-        console.write("Your " + (service.loseWin()?"win":"lose") + ".");
-    }
+public interface Quiz {
+    void startQuiz();
 }
