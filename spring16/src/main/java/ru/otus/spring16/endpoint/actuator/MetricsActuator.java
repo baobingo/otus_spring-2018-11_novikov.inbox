b6eabdd8e92b5fcd56178796c642444b31129e7b
baobingo <novikov.inbox@gmail.com>
2019-04-08 08:55:11 +0300

    spring16

--- spring16/src/main/java/ru/otus/spring16/endpoint/actuator/MetricsActuator.java
+++ spring16/src/main/java/ru/otus/spring16/endpoint/actuator/MetricsActuator.java
@@ -23,18 +23,31 @@ public class MetricsActuator {
     @ReadOperation
     public Map<String, Object> info(){
         result = new HashMap<>();
-        Mono<Integer> reviewCount = bookRepositoryReactive.findAll().map(book -> book.getReviews().size()).reduce((integer, integer2) -> integer + integer2);
-        Mono<Long> bookCount = bookRepositoryReactive.count();
-        Mono<Long> bookCountWoutReviews = bookRepositoryReactive.findAll().filter(book -> book.getReviews().size() == 0).count();
-        int mostCommentedBook = bookRepositoryReactive.findAll().sort((book1, book2)->
-                book1.getReviews().size() > book2.getReviews().size() ? book1.getReviews().size() : book2.getReviews().size()
-        ).take(1).blockFirst().getId();
 
-        result.put("Total reviews count", String.valueOf(reviewCount.block()));
-        result.put("Total books", String.valueOf(bookCount.block()));
-        result.put("Total count of book w/out reviews", String.valueOf(bookCountWoutReviews.block()));
-        result.put("ID of most commented book", String.valueOf(mostCommentedBook));
+        result.put("Total reviews count", String.valueOf(getReviewCount()));
+        result.put("Total books", String.valueOf(getBookCount()));
+        result.put("Total count of book w/out reviews", String.valueOf(getBookCountWoutReviews()));
+        result.put("ID of most commented book", String.valueOf(getMostCommentedBook()));
 
         return result;
     }
+
+    private Integer getReviewCount(){
+        return bookRepositoryReactive.findAll().map(book -> book.getReviews().size()).defaultIfEmpty(0).reduce((integer, integer2) -> integer + integer2).block();
+    }
+
+    private Long getBookCount(){
+        return bookRepositoryReactive.count().defaultIfEmpty(0L).block();
+    }
+
+    private Long getBookCountWoutReviews(){
+        return bookRepositoryReactive.findAll().filter(book -> book.getReviews().size() == 0).count().defaultIfEmpty(0L).block();
+    }
+
+    private int getMostCommentedBook(){
+        return bookRepositoryReactive.findAll().sort((book1, book2)->
+                book1.getReviews().size() > book2.getReviews().size() ? book1.getReviews().size() : book2.getReviews().size()
+        ).take(1).blockFirst().getId();
+    }
+
 }
